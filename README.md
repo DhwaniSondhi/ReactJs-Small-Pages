5f7515484c07edd2ca187390d096999123901ec9
DSondhi <dhwanisondhi3@gmail.com>
2019-06-04 00:49:29 -0400

    Adding all the methods, decription and gif

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This repository contains basic web-pages made using ReactJS.
 
 Following are some of the basic web-pages:
 - [Shopping Cart page of e-commerce website](https://github.com/DhwaniSondhi/ReactJs-Small-Pages/tree/master/counter-app)
+- [Calculator App](https://github.com/DhwaniSondhi/ReactJs-Small-Pages/tree/master/calculator-app)
--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -7,30 +7,105 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    input: "",
-    prevOperand: "",
+    input: "0",
     operator: ""
   };
   handleAdd = value => {
-    this.state.prevOperand = this.state.input;
-    this.setState({ input: "" });
-    this.state.operator = "+";
+    let newVal = this.handleEqualTo();
+    if (newVal) {
+      this.setState({ operator: "+" });
+      this.setState({ input: newVal + "+" });
+    } else if (this.state.operator.length === 0) {
+      this.setState({ operator: "+" });
+      this.setState({ input: this.state.input + "+" });
+    }
   };
-  handleEqual = value => {};
+  handleMultiply = value => {
+    let newVal = this.handleEqualTo();
+    if (newVal) {
+      this.setState({ operator: "*" });
+      this.setState({ input: newVal + "*" });
+    } else if (this.state.operator.length === 0) {
+      this.setState({ operator: "*" });
+      this.setState({ input: this.state.input + "*" });
+    }
+  };
+  handleSubtract = value => {
+    let newVal = this.handleEqualTo();
+    if (newVal) {
+      this.setState({ operator: "-" });
+      this.setState({ input: newVal + "-" });
+    } else if (this.state.operator.length === 0) {
+      this.setState({ operator: "-" });
+      this.setState({ input: this.state.input + "-" });
+    }
+  };
+  handleDivide = value => {
+    let newVal = this.handleEqualTo();
+    if (newVal) {
+      this.setState({ operator: "/" });
+      this.setState({ input: newVal + "/" });
+    } else if (this.state.operator.length === 0) {
+      this.setState({ operator: "/" });
+      this.setState({ input: this.state.input + "/" });
+    }
+  };
   handleButtonClick = value => {
-    this.setState({ input: this.state.input + value });
+    if (this.state.input == "0") {
+      this.setState({ input: value });
+    } else {
+      this.setState({ input: this.state.input + value });
+    }
   };
   handleClearClick = () => {
-    this.setState({ input: "" });
+    this.setState({ input: "0" });
   };
   handleZeroClick = value => {
     if (this.state.input.length != 0)
       this.setState({ input: this.state.input + value });
   };
   handleDecimalClick = () => {
+    console.log(this.state.input);
     if (this.state.input.length === 0) this.setState({ input: "0." });
     else if (this.state.input.indexOf(".") === -1) {
       this.setState({ input: this.state.input + "." });
+    } else if (this.state.operator.length > 0) {
+      if (this.state.input.split(this.state.operator)[1]) {
+        this.setState({ input: this.state.input + "." });
+      } else {
+        this.setState({ input: this.state.input + "0." });
+      }
+    }
+  };
+  handleEqualTo = () => {
+    let secondInp = this.state.input.split(this.state.operator)[1];
+    let firstInp = this.state.input.split(this.state.operator)[0];
+    if (secondInp) {
+      let newVal = 0;
+      if (firstInp.indexOf(".") > -1 || secondInp.indexOf(".") > -1) {
+        firstInp = parseFloat(this.state.input.split(this.state.operator)[0]);
+        secondInp = parseFloat(this.state.input.split(this.state.operator)[1]);
+      } else {
+        firstInp = parseInt(this.state.input.split(this.state.operator)[0]);
+        secondInp = parseInt(this.state.input.split(this.state.operator)[1]);
+      }
+
+      switch (this.state.operator) {
+        case "+":
+          newVal = firstInp + secondInp;
+          break;
+        case "*":
+          newVal = firstInp * secondInp;
+          break;
+        case "/":
+          newVal = firstInp / secondInp;
+          break;
+        case "-":
+          newVal = firstInp - secondInp;
+          break;
+      }
+      this.setState({ operator: "", input: "" + newVal });
+      return newVal;
     }
   };
   render() {
@@ -44,13 +119,13 @@ class App extends Component {
             <Button onClick={this.handleButtonClick}>7</Button>
             <Button onClick={this.handleButtonClick}>8</Button>
             <Button onClick={this.handleButtonClick}>9</Button>
-            <Button onClick={this.handleButtonClick}>/</Button>
+            <Button onClick={this.handleDivide}>/</Button>
           </div>
           <div className="row">
             <Button onClick={this.handleButtonClick}>4</Button>
             <Button onClick={this.handleButtonClick}>5</Button>
             <Button onClick={this.handleButtonClick}>6</Button>
-            <Button onClick={this.handleButtonClick}>*</Button>
+            <Button onClick={this.handleMultiply}>*</Button>
           </div>
           <div className="row">
             <Button onClick={this.handleButtonClick}>1</Button>
@@ -61,8 +136,8 @@ class App extends Component {
           <div className="row">
             <Button onClick={this.handleDecimalClick}>.</Button>
             <Button onClick={this.handleZeroClick}>0</Button>
-            <Button onClick={this.handleEqual}>=</Button>
-            <Button onClick={this.handleButtonClick}>-</Button>
+            <Button onClick={this.handleEqualTo}>=</Button>
+            <Button onClick={this.handleSubtract}>-</Button>
           </div>
           <div className="row">
             <ClearButton onClick={this.handleClearClick}>Clear</ClearButton>
